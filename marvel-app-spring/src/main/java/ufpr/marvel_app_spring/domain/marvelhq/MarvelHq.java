b2f1567f29f5af04c30f01a286d51c69f5522ef8
Albano Maywitz <allbano@gmail.com>
2024-06-23 04:04:20 -0300

    Atualizado MarvelHq: adicionado o record e o Repository para classe.

--- marvel-app-spring/src/main/java/ufpr/marvel_app_spring/domain/marvelhq/MarvelHq.java
+++ marvel-app-spring/src/main/java/ufpr/marvel_app_spring/domain/marvelhq/MarvelHq.java
@@ -20,14 +20,27 @@ public class MarvelHq {
 	
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "hq_id")
+	@Column(name = "hq_id",nullable = false)
 	private Long id;
-	@Column(name = "hq_titulo")
+	@Column(name = "hq_titulo",nullable = false)
 	private String titulo;
-	@Column(name = "hq_imagem")
+	@Column(name = "hq_imagem",nullable = false)
 	private String imagem;
-	@Column(name = "hq_data_devolucao")
+	@Column(name = "hq_data_devolucao",nullable = false)
 	private LocalDate data_devolucao;
 
+	public MarvelHq(RequestCreateMarvelHqDto requestMarvelHqDto) {
+		this.titulo = requestMarvelHqDto.titulo();
+		this.imagem = requestMarvelHqDto.imagem();
+		this.data_devolucao = requestMarvelHqDto.dataDevolucao();
+		
+	}
 
+	@Override
+	public String toString() {
+		return "MarvelHq [id=" + id + ", titulo=" + titulo + ", imagem=" + imagem + ", data_devolucao=" + data_devolucao
+				+ "]";
+	}
+	
+	
 }
---
+++ marvel-app-spring/src/main/java/ufpr/marvel_app_spring/domain/marvelhq/MarvelHqRepository.java
@@ -0,0 +1,7 @@
+package ufpr.marvel_app_spring.domain.marvelhq;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarvelHqRepository extends JpaRepository<MarvelHq, Long> {
+
+}
